a2fed705640e832ee570540ca142c59b5b26c806
Christian Bartsch <christian.bartsch@v-play.net>
2020-04-08 17:33:19 +0200

    Update Felgo libs and headers

--- include/FelgoIOSView.h
+++ include/FelgoIOSView.h
@@ -1,5 +1,6 @@
 //
-//  FelgoIOS.h
+//  FelgoIOSView.h
+//  EmbeddedVPlayTest
 //
 //  Created by Christian Bartsch on 12.07.18.
 //
@@ -8,11 +9,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef void (^initBlock)();
+typedef void (^signalHandler)(NSArray *params);
+
 @interface FelgoIOSView : UIView
 
 @property (nonatomic, retain) NSURL *qmlSource;
 
 @property (nonatomic, retain) NSData *qmlContent;
+
+// optional code block to be called when the QML file is instantiated
+// called before QML Component.onComplete is emitted
+@property (nonatomic) initBlock qmlInitBlock;
+
+- (void)setQmlProperty:(NSString *)propertyName value:(id)value;
+
+- (id)getQmlProperty:(NSString *)propertyName;
+
+- (void)callQmlMethod:(NSString *)methodName value:(NSArray *)params;
+
+- (void)addSignalHandler:(NSString *)signalName handler:(signalHandler)handler;
 
 @end
 
